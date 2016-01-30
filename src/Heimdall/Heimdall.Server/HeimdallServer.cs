6222cf69fac3bb8442f3a32c99d3f1f911377f40
Alexandre Kerametlian <akerametlian@live.com>
2016-01-30 14:08:24 -0800

    Create the Visual Studio projects for Heimdall server and Windows UWP client.

---
+++ src/Heimdall/Heimdall.Server/HeimdallServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Heimdall.Server
+{
+    /// <summary>
+    /// The main class implementing Heimdall server logic.
+    /// </summary>
+    public class HeimdallServer
+    {
+    }
+}
