748cf016a5138b33f2513a6255fbf6d889151747
Alexandre Kerametlian <akerametlian@live.com>
2017-03-12 00:14:57 -0800

    Add basic HTTP server capabilities to Heimdall Server

---
+++ src/Heimdall/Heimdall.Server/HeimdallServerConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Heimdall.Server
+{
+    /// <summary>
+    /// Encapsulates configuration settings for a Heimdall server.
+    /// </summary>
+    public class HeimdallServerConfig
+    {
+        /// <summary>
+        /// The network port number to listen on for requests.
+        /// </summary>
+        public UInt16 Port { get; set; }
+
+        /// <summary>
+        /// The maximum number of concurrect connections to accept.
+        /// </summary>
+        public int MaxConnections { get; set; }
+    }
+}
--- src/Heimdall/Heimdall.ServerHost/Program.cs
+++ src/Heimdall/Heimdall.ServerHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Heimdall.Server;
 
 namespace Heimdall.ServerHost
 {
@@ -10,6 +11,14 @@ namespace Heimdall.ServerHost
     {
         static void Main(string[] args)
         {
+            HeimdallServerConfig config = new HeimdallServerConfig()
+            {
+                Port = 11337,
+                MaxConnections = 4,
+            };
+
+            HeimdallServer server = new HeimdallServer(config, new Logger());
+            server.Run();
         }
     }
 }
