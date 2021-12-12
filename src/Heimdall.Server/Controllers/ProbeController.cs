85d6e9641be160a97f8835aca1e6d8d09224dc24
Alexandre Kerametlian <akerametlian@outlook.com>
2021-12-11 19:52:47 -0800

    Create new Net6 Heimdall Server

---
+++ src/Heimdall.Server/Controllers/ProbeController.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Alexandre Kerametlian.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Reflection;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Heimdall.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProbeController : ControllerBase
+{
+    private ILogger<ProbeController> Logger { get; init; }
+
+    public ProbeController(ILogger<ProbeController> logger)
+    {
+        this.Logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        await Task.Yield();
+        return this.Ok(ProbeResponse.Default);
+    }
+
+    private class ProbeResponse
+    {
+        public static ProbeResponse Default { get; } = new ProbeResponse();
+
+        public string ProductName { get; init; }
+
+        public string Version { get; init; }
+
+        private ProbeResponse()
+        {
+            var assembly = typeof(ProbeController).Assembly;
+
+            this.ProductName =
+                assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title
+                ?? string.Empty;
+
+            this.Version = 
+                assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                ?? string.Empty;
+        }
+    }
+}
---
+++ src/Heimdall.Server/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Alexandre Kerametlian.
+// Licensed under the Apache License, Version 2.0.
+
+namespace Heimdall.Server;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureDIContainerServices(builder.Services);
+
+        var app = builder.Build();
+        ConfigureApplicationPipeline(app);
+
+        app.Run();
+    }
+
+    public static void ConfigureDIContainerServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public static void ConfigureApplicationPipeline(WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
