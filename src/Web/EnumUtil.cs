2b4de2f3b35b9caf7894a22c597b836b4c7f3dc3
Alexandre Kerametlian <akerametlian@outlook.com>
2023-01-09 00:31:57 -0800

    Implement device admin view that shows all devices

---
+++ src/Web/EnumUtil.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Alexandre Kerametlian.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace Heimdall.Web;
+
+public static class EnumUtil<TEnum> where TEnum : Enum
+{
+    private static readonly ConcurrentDictionary<TEnum, string> PrettyStringCache
+        = new ConcurrentDictionary<TEnum, string>();
+
+    public static string ToPrettyString(TEnum value)
+        => PrettyStringCache.GetOrAdd(value, v => AddSpacesToCamelCase(v.ToString()));
+
+    private static string AddSpacesToCamelCase(string s)
+    {
+        var builder = new StringBuilder(s.Length);
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsUpper(s[i]))
+            {
+                builder.Append(' ');
+            }
+            builder.Append(s[i]);
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ src/Web/Shared/DeviceTable.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using Heimdall.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Heimdall.Web.Shared;
+
+public partial class DeviceTable
+{
+    [Inject]
+    private HttpClient Http { get; set; }
+
+    private List<Device> devices;
+
+    protected async override Task OnInitializedAsync()
+    {
+        try
+        {
+            this.devices = await this.Http.GetFromJsonAsync<List<Device>>(
+                "api/admin/devices",
+                options: Helpers.DefaultJsonOptions);
+        }
+        catch (AccessTokenNotAvailableException exception)
+        {
+            exception.Redirect();
+        }
+    }
+
+    private static string DeviceTypeToIconClass(DeviceType type)
+        => type switch
+        {
+            DeviceType.Unknown => "oi-question-mark",
+            DeviceType.ShellySwitch => "oi-pulse",
+            _ => "oi-question-mark",
+        };
+}
