37710dd6c0dc70d8a333cace9ca3162b1e58ac65
Alexandre Kerametlian <akerametlian@outlook.com>
2023-01-21 14:27:03 -0800

    Adjust deployment scripts

--- deployment/publish.cmd
+++ deployment/publish.cmd
@@ -4,8 +4,13 @@ pushd %~dp0..
 
 echo Building..
 dotnet publish -c Retail --runtime linux-arm64 --self-contained -o .\out src\Server\Heimdall.Server.csproj
+dotnet publish -c Retail --runtime linux-arm64 --self-contained -o .\out src\WebhookProxy\Heimdall.WebhookProxy.csproj
 copy deployment\heimdall.service out
+copy deployment\heimdall-webhookproxy.service out
 copy deployment\deploy.sh out
+
+choice /M "Copy to HomePi staging?"
+IF ERRORLEVEL ==2 GOTO END
 
 echo Copying to staging directory on HomePi..
 scp -r out/* pi@homepi.local:/home/pi/staging/heimdall/
