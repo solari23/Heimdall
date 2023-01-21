01784ca2a3e956973cdc9c18fcdd3341ab0cdefc
Alexandre Kerametlian <akerametlian@outlook.com>
2023-01-21 13:57:25 -0800

    Add webhookproxy project to solution

---
+++ src/WebhookProxy/Program.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Alexandre Kerametlian.
+// Licensed under the Apache License, Version 2.0.
+
+namespace Heimdall.WebhookProxy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+        app.MapControllers();
+        app.Run();
+    }
+}
